7008faec8674226cd24fc58fc0e198ac9031a9a5
ChaL <chaluntonvipusanapas@gmail.com>
2025-03-05 15:10:30 +0700

    built-in-array-function

--- ex-5.js
+++ ex-5.js
@@ -7,6 +7,8 @@ const students = [
 
 function getAverageStudentScore(students) {
   // Start coding here
+  const totalScore = students.reduce((total, student) => total + student.score, 0)
+  return totalScore / students.length;
 }
 
-getAverageStudentScore(students); // Output: 87.5
+console.log(getAverageStudentScore(students)); // Output: 87.5
