ec60c045b31654229e2d2cd1459a0450242319f4
Pongsakon <charlee2543@hotmail.com>
2025-03-05 13:57:44 +0700

    feat:charlee add ex1

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,17 @@
+// เรียกดูindex ;
+
 let words = ["hello", "world"];
 
 function getWordLengths(words) {
-  // Start coding here
+	// Start coding here
+	let numberWords = [];
+	words.map((words) => {
+		numberWords.push(words.length);
+		// console.log("words: ", words);
+	});
+	// console.log("words: ", words);
+	// console.log("numberWords: ", numberWords);
+	return numberWords;
 }
 
 const result = getWordLengths(words);
